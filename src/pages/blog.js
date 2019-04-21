294e6203bad6c3e0447bacdfa627a0a30a4afd95
Kit Macleod <kitmacleod@gmail.com>
2019-04-21 15:08:16 +0100

    Adding contentful API

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,20 +5,35 @@ import Layout from '../components/layout'
 import blogStyles from './blog.module.scss'
 
 
+// Markdown
+// const BlogPage =() => {
+//   const data = useStaticQuery(graphql`
+//   query {
+//     allMarkdownRemark {
+//       edges {
+//         node {
+//           frontmatter {
+//             title
+//             date
+//           }
+//           fields {
+//             slug
+//           }
+//         }
+//       }
+//     }
+//     }
+// `)
 
 const BlogPage =() => {
   const data = useStaticQuery(graphql`
   query {
-    allMarkdownRemark {
+    allContentfulBlogPost ( sort: {fields: publishedDate, order: DESC }) {
       edges {
         node {
-          frontmatter {
             title
-            date
-          }
-          fields {
             slug
-          }
+            publishedDate (formatString: "MMMM Do, YYYY")
         }
       }
     }
@@ -30,12 +45,12 @@ console.log(data)
     <Layout>
       <h1>Blog</h1>
       <ol className={blogStyles.posts}>
-        {data.allMarkdownRemark.edges.map((edge) => {
+        {data.allContentfulBlogPost.edges.map((edge) => {
           return (
             <li className={blogStyles.post}>
-              <Link to={`/blog/${edge.node.fields.slug}`}>
-              <h2>{edge.node.frontmatter.title}</h2>
-              <p>{edge.node.frontmatter.date}</p>
+              <Link to={`/blog/${edge.node.slug}`}>
+              <h2>{edge.node.title}</h2>
+              <p>{edge.node.publishedDate}</p>
               </Link>        
             </li>
           )
