cf82c4f34454ae668801f04bd0621479446d22e9
Kit Macleod <kitmacleod@gmail.com>
2019-05-25 09:06:19 +0100

    removed blog component and related code

--- src/components/header.js
+++ src/components/header.js
@@ -29,9 +29,7 @@ const Header =() => {
           <li>
             <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">Home</Link>
           </li>  
-          <li>
-            <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Blog</Link>
-          </li>
+        
           <li>
             <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">About</Link>
           </li>
