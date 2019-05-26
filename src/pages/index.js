00bb3d113e4a7880b1d9943c545af48bf4c3ff67
Kit Macleod <kitmacleod@gmail.com>
2019-05-26 11:56:19 +0100

    Revised home page text and css

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,11 +7,11 @@ const IndexPage = () =>  {
   return(
     <Layout>
     <Head title="Home" />
-    <h1>Research scientist and full-stack develop </h1>
-    <h2>What I study: land and water resource systems under environmental change</h2>
-    <h2>How: by developing spatial apps with stakeholders, using data science and participatory technologies</h2>
-    <h2>Why: to improve understanding of their functioning, use and management</h2>
-    <h2> </h2>  
+    <h2>I am a research scientist and full-stack developer. </h2>
+    <h3>I study land and water resource systems under environmental change.</h3>
+    <h3>How: by developing spatial apps with stakeholders, using data science and participatory technologies.</h3>
+    <h3>Why: to improve our collective understanding of their functioning, use and management.</h3>
+      
  
     
     </Layout>
